221de0ff45585ef1aa9d68614072cadc261f1690
MartianFlow <jeffersonardila@outlook.com>
2023-08-01 20:40:43 -0500

    Actualizacion comentarios

--- src/test/scala/videogamedb/scriptfundamentals/AddPauseTime.scala
+++ src/test/scala/videogamedb/scriptfundamentals/AddPauseTime.scala
@@ -15,15 +15,16 @@ class AddPauseTime extends Simulation {
 
     .exec(http("Get all video games - 1st call")
     .get("/videogame"))
-    .pause(5)
+    .pause(5) //Podemos agregar pausas directamente con valor. Por defecto es tomado en milisegundos
 
     .exec(http("Get specific game")
     .get("/videogame/1"))
-    .pause(1, 10)
+    .pause(1, 10) //Tambien podemos manejar rangos entre varios valores. aqui pausara en un valor aleatorio entre 1 a 10 segundos
 
     .exec(http("Get all video games - 2nd call")
     .get("/videogame"))
-    .pause(3000.milliseconds)
+    .pause(3000.milliseconds) //Tambien podremos indicar otras medidas de tiempo, en este caso indicamos que se pausara 3000 milisegundos
+    //Para ello debemos usar la clase DurationInt
 
   setUp(
     scn.inject(atOnceUsers(1))
