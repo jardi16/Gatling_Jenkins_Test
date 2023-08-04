59c651de826258786d944e9afd5fdd4f1dc37172
MartianFlow <jeffersonardila@outlook.com>
2023-08-04 11:20:53 -0500

    Actualizacion comentarios

--- src/test/scala/videogamedb/feeders/CsvFeeder.scala
+++ src/test/scala/videogamedb/feeders/CsvFeeder.scala
@@ -8,14 +8,16 @@ class CsvFeeder extends Simulation {
   val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
     .acceptHeader("application/json")
 
-  val csvFeeder = csv("data/gameCsvFile.csv").circular
+  val csvFeeder = csv("data/gameCsvFile.csv").circular //Para pasar datos desde un archivo .csv utilizamos el metodo csv("ruta de archivo") e indicamos como se obtendran dichos datos
+  //.circular nos sirve para obtener los datos de forma repetitiva
+  //.random nos sirve para  obtener los datos de forma aleatoria
 
   def getSpecifcVideoGame() = {
     repeat(10) {
-      feed(csvFeeder)
-        .exec(http("Get video game with name - #{gameName}")
-        .get("/videogame/#{gameId}")
-        .check(jsonPath("$.name").is("#{gameName}"))
+      feed(csvFeeder) //Este metodo nos sirve para indicar el archivo .scv que utilizaremos
+        .exec(http("Get video game with name - #{name}") //Para utilizar los parametros del archivo csv, lo hacemos mediante #{parametro}
+        .get("/videogame/#{gameId}") //
+        .check(jsonPath("$.name").is("#{name}"))
         .check(status.is(200)))
         .pause(1)
     }
